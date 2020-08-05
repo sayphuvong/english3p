de24d258b31c68ada19e8117fe34354f5db86ddd
Say Phu Vong <phuvong@techvsi.com>
2020-08-05 22:52:39 +0700

    Updates

---
+++ precache-manifest.e1d67eb78cee659f7f3115349dd57c63.js
@@ -0,0 +1,72 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/english3p/074332cebe7bd62e113fb7a515d437c6.jpg"
+  },
+  {
+    "url": "/english3p/0e61ed706e4539cd65b9caf3ddd5994f.jpg"
+  },
+  {
+    "url": "/english3p/1e666e164e55d6bfefc59041b82c413e.jpg"
+  },
+  {
+    "url": "/english3p/24c216c519f5a6d9dad1e0429cb597dd.png"
+  },
+  {
+    "url": "/english3p/43e24b0da247cd17d74bec682e9d600b.png"
+  },
+  {
+    "url": "/english3p/559707d3a793aa8017069a013f9c02d3.png"
+  },
+  {
+    "url": "/english3p/5747e4eda354c52c67dbc6ead57e181e.jpg"
+  },
+  {
+    "url": "/english3p/9873d3f1b0458f104543babac4d48a2a.jpg"
+  },
+  {
+    "url": "/english3p/9e89420d5a7a2054f01c9dca8fa8e03c.jpg"
+  },
+  {
+    "url": "/english3p/a03cba5ffaa0bbc66bed1bf1568267f1.png"
+  },
+  {
+    "url": "/english3p/ada6ef8f2fe2f607eabc0ac3b61598c0.png"
+  },
+  {
+    "url": "/english3p/caaf3fef2697e3475fa25606afd099d8.png"
+  },
+  {
+    "url": "/english3p/db36d5c001f876ff11c9784e6e17bd8f.png"
+  },
+  {
+    "url": "/english3p/eefd6d516f56623db3a7494e712fabe0.png"
+  },
+  {
+    "revision": "bd219c2ac453b460b687d9402422535f",
+    "url": "/english3p/index.html"
+  },
+  {
+    "revision": "88d295492d4046d33063",
+    "url": "/english3p/static/css/main.84876d76.chunk.css"
+  },
+  {
+    "revision": "fac495ce4adb247da513",
+    "url": "/english3p/static/js/2.104c5c18.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/english3p/static/js/2.104c5c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d295492d4046d33063",
+    "url": "/english3p/static/js/main.c047b3e3.chunk.js"
+  },
+  {
+    "revision": "fcf2cac37d85a797f319",
+    "url": "/english3p/static/js/runtime-main.15eadeb8.js"
+  },
+  {
+    "revision": "f0123999f7c1455181bd5cdaa22136e2",
+    "url": "/english3p/static/media/spinner-loading.f0123999.svg"
+  }
+]);
